d33a3b0d4fcaefa7714d384d90c5dd0858544ad8
johnsonlee <g.johnsonlee@gmail.com>
2019-10-09 14:49:00 +0800

    Add device test

---
+++ src/androidTest/java/io/github/boostersamples/MainActivityTest.kt
@@ -0,0 +1,25 @@
+package io.github.boostersamples
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun test() {
+        onView(withId(R.id.activity_main_greeting)).perform(click())
+    }
+
+}
--- src/main/java/io/github/boostersamples/MainActivity.kt
+++ src/main/java/io/github/boostersamples/MainActivity.kt
@@ -1,13 +1,34 @@
 package io.github.boostersamples
 
+import android.app.Activity
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+import java.util.concurrent.Executors
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity(), View.OnClickListener {
+
+    private val executor = Executors.newSingleThreadExecutor()
+
+    private lateinit var txtGreeting: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.txtGreeting = findViewById(R.id.activity_main_greeting)
+        this.txtGreeting.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.activity_main_greeting -> {
+                executor.execute {
+                    println(">>>>" + System.currentTimeMillis())
+                    Thread.sleep(5000L)
+                    println("<<<<" + System.currentTimeMillis())
+                }
+            }
+        }
     }
 
 }
