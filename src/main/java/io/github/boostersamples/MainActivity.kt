9c79c1b34c4bed139df15df1b693c146c8b6cc22
johnsonlee <g.johnsonlee@gmail.com>
2019-10-26 12:12:20 +0800

    Update sample code

--- src/main/java/io/github/boostersamples/MainActivity.kt
+++ src/main/java/io/github/boostersamples/MainActivity.kt
@@ -2,33 +2,24 @@ package io.github.boostersamples
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import java.util.concurrent.Executors
+import android.widget.AbsListView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : Activity(), View.OnClickListener {
-
-    private val executor = Executors.newSingleThreadExecutor()
-
-    private lateinit var txtGreeting: TextView
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        this.txtGreeting = findViewById(R.id.activity_main_greeting)
-        this.txtGreeting.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View) {
-        when (v.id) {
-            R.id.activity_main_greeting -> {
-                executor.execute {
-                    println(">>>>" + System.currentTimeMillis())
-                    Thread.sleep(5000L)
-                    println("<<<<" + System.currentTimeMillis())
-                }
+        activity_main_greeting.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, (0..100).map { "$it" })
+        activity_main_greeting.setOnScrollListener(object : AbsListView.OnScrollListener {
+            override fun onScroll(view: AbsListView?, firstVisibleItem: Int, visibleItemCount: Int, totalItemCount: Int) {
+                // Thread.sleep(100)
             }
-        }
+
+            override fun onScrollStateChanged(view: AbsListView?, scrollState: Int) {
+            }
+        })
     }
 
 }
